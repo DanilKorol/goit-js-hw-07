3e84a258e6d09275b71b03db79cdfb1445f1e312
Danilo Korol <777danilkorol777@gmail.com>
2022-11-14 18:34:02 +0200

    add <li>

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,9 +10,9 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 function createGalleryMarkup(galleryItems) {
      return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
+</a></li>`
     })
     .join("");
 };
